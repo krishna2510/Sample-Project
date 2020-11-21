2ac1cc8c318723e1a38448d66c5f22bae046004d
krishna2510 <sskrishna25@gmail.com>
2020-11-21 12:32:22 +0530

    srini work

--- src/test/java/org/locator/Sample1.java
+++ src/test/java/org/locator/Sample1.java
@@ -2,6 +2,7 @@ package org.locator;
 
 import java.io.IOException;
 
+import org.apache.poi.ss.usermodel.Workbook;
 import org.sample.BaseClass;
 
 public class Sample1 extends BaseClass{
@@ -13,5 +14,11 @@ public static void main(String[] args) throws IOException {
 	enterText(log.getUsername(), getDataFromExcel("Sheet1", 1, 0));
 	enterText(log.getPassword(), getDataFromExcel("Sheet1", 1, 1));
 	elementClick(log.getLoginbtn());
+	
+	  
+}
+ private void project() {
+	System.out.println("updated srini work");
+	System.out.println("work done");
 }
 }
